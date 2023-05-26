e2f6e75be4a9f299b49016b15a078e018a28baeb
Alex <afanasbev1@gmail.com>
2023-05-26 16:35:17 +0300

    upd

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,13 +5,11 @@ import { Header, Wrapper } from './App.styled';
 import { Form } from '../Form';
 import { nanoid } from 'nanoid';
 
-//nanoid()
-
 export class App extends Component {
   state = {
     products: [
       { id: 'id-1', text: 'Baget', quantity: 1, completed: true },
-      { id: 'id-2', text: 'Milk', quantity: 1,completed: false },
+      { id: 'id-2', text: 'Milk', quantity: 1, completed: false },
       { id: 'id-3', text: 'Chocolate', quantity: 1, completed: false },
     ],
   };
@@ -24,7 +22,7 @@ export class App extends Component {
 
   formSubmitHandler = data => {
     console.log(data);
-    const newProduct = { 
+    const newProduct = {
       id: nanoid(),
       text: data.productName,
       quantity: data.productQuantity,
@@ -53,6 +51,34 @@ export class App extends Component {
       }),
     }));
   };
+  /////////////////////// Жизненный цикл ////////////////////////////////////////
+  // Стадия монтажа - componentDidMount  - вызывается один раз при создании компонента
+  // При стадии монтажа можно прочитать значения из LOCALSTORAGE. (Получить начальные данные)
+  componentDidMount() {
+    console.log('Это стадия монтажа, делаю проверку в localStorage');
+    const productList = localStorage.getItem('PRODUCTS');
+    const parsedProductList = JSON.parse(productList);
+    // Проверка localStorage на наличие данных с прошлой сессии, если данных нет, то зарендерится по умолчанию state:
+    if (parsedProductList) {
+      this.setState({ products: parsedProductList });
+    }
+    console.log(parsedProductList);
+  }
+  // Стадия обновления:
+  //- componentDidUpdate - вызывается каждый раз при обновлении компонента (при новых пропсах или изменении стейта) Если делаем setState то проверить что бы поле обновилось иначе зациклится компонент
+  // componentDidUpdate(prevProps, prevState) принимает преведучещее значение props и state (значения до обновления)
+  // можно делать проверку для записи в localStorage, если при обновлении компонента this.state.products !== prevState.state.products сравнение массивов по ссылке
+  // если они не равны то products был обновлен
+  componentDidUpdate(prevProps, prevState) {
+    console.log('App componentDidUpdate');
+    if (this.state.products !== prevState.products) {
+      console.log('Update products');
+      localStorage.setItem('PRODUCTS', JSON.stringify(this.state.products));
+    }
+  }
+
+  //- getSnapshotBeforeUpdate - (использовать для привязки каретки скрола при добавлении данных на странице)
+  // Стадия размонтирования - componentWillUnmount - при удалении компонента из DOM.
 
   render() {
     const { products } = this.state;
@@ -69,7 +95,6 @@ export class App extends Component {
       0
     ); //Вычисляемое значение
 
-    ///////////////////////////////////////////////////////////////
     return (
       <Wrapper>
         <Header>Продуктовый список</Header>
