f82b50d7c084c012dee6b75f9a2aec8813110dd4
Alex <afanasbev1@gmail.com>
2023-05-29 16:13:52 +0300

    upd: fix modal window

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 export const ModalFixed = styled.div`
+ 
   position: fixed;
   top: 0;
   left: 0;
   width: 100vw;
-  height: 100vw;
+  height: 100vh;
   background-color: rgba(0, 0, 0, 0.5);
 `;
 
